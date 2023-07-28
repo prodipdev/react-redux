d4725006149ea60d4f62059e391d89501b11cb8f
prodipdev <prodipink@gmail.com>
2023-07-28 19:47:56 +0600

    Initialy setup react-redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/counter";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
